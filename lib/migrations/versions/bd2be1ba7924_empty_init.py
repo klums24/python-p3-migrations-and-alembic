2337532058b91ebe2abb24cb2d1572d1b63854da
Kevin Lumauig <klums24@gmail.com>
2023-06-13 10:33:56 -0700

    migrations and alembic

---
+++ lib/migrations/versions/bd2be1ba7924_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: bd2be1ba7924
+Revises: 
+Create Date: 2023-06-13 10:23:14.361925
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bd2be1ba7924'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
---
+++ lib/migrations/versions/c7804fd990ce_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: c7804fd990ce
+Revises: bd2be1ba7924
+Create Date: 2023-06-13 10:27:57.903283
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c7804fd990ce'
+down_revision = 'bd2be1ba7924'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
